a23f5b46d07e930f8bb2e3b3e89d1524d1b647c7
Chanane-Walid <walid.chanane2@gmail.com>
2024-11-19 15:17:11 +0100

    commit

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,11 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+	float size = w * h * 3;
+	if (strcmp(unit, "bt") == 0) return size * 8;
+	if (strcmp(unit, "ko") == 0) return (float) size / 1024;
+	if (strcmp(unit, "mo") == 0) return (float) size / (1024 * 1024);
+	if (strcmp(unit, "go") == 0) return (float) size / (1024 * 1024 * 1024);
    // YOUR CODE HERE - END
    return 0;
 }
--- video.c
+++ video.c
@@ -10,7 +10,11 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+	float size = w * h * (durationMovie*3 + durationCredits) * fps;
+	if(strcmp(unit,"bt")==0) return size*8;
+	if(strcmp(unit,"ko")==0) return (float) size / 1024;
+	if(strcmp(unit,"mo")==0) return (float) size / (1024 * 1024);
+	if(strcmp(unit,"go")==0) return (float) size / (1024 * 1024 * 1024);
    // YOUR CODE HERE - END
    return 0;
 }
